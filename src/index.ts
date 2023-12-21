f37828c1ef82665ec094df1124d8c76ba9089505
FarzeenKist <farzeenkistnareddy@gmail.com>
2023-12-21 14:10:40 +0400

    Improve error messages

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const CaesarCipher = Canister({
   // calling the encrypt function
   Encrypt : query([text, nat8], Result(text, text), (text, shift) => {
     if (shift == 0 || shift > 25) {
-      return Err('Shift parameter must be a valid integer');
+      return Err('Shift arugment must be between 1 and 25');
     }
     return Ok(cipher.encryptCaesar(text, shift));
   }),
@@ -15,7 +15,7 @@ const CaesarCipher = Canister({
   // calling the decrypt function
   Decrypt: query([text, nat8], Result(text, text), (text, shift) => {
     if (shift == 0 || shift > 25) {
-      return Err('Shift parameter must be a valid integer');
+      return Err('Shift arugment must be between 1 and 25');
     }
     return Ok(cipher.decryptCaesar(text, shift));
   }),
